ee78395bfb7b8f8530e61c35c276bec0fba47a08
Karina Akemy <yakankuro@hotmail.com>
2020-03-25 12:12:19 -0300

    atualização back-end

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors'); 
 const routes = require('./routes').default;
 var mainRoutes = require('./Routes.js')
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(mainRoutes)
 
--- routes.js
+++ routes.js
@@ -1,27 +1,20 @@
 const express = require('express');
-const crypto = require('crypto');
-
-const connection = require('./database/connection');
-
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
+//routes.get('/ongs' , async function(request, response){
+   // const ongs = await connection('ongs').select('*');
+    //return response.json(ongs);
+//});
 
-routes.post('/ongs', async function (request, response) {
-    const  {name, email, whatsapp, city, uf} = request.body;
-
-    const id = crypto.randomBytes(4).toString('HEX');
-    
-     await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    })
-
-    return  response.json({id});
-
-});
-
+routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.index);
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/profile', ProfileController.index);
+routes.post('/sessions', SessionController.create);
 module.exports = routes;
